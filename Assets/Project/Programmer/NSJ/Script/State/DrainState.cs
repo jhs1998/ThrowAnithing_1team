dd69746db1b8c07685037809b9e92e9f5ba0ea72
SeungJu0406 <nss8164@gmail.com>
2024-12-20 19:54:24 +0900

    [Feat] 드레인 애니메이션 종료 조건 변경

--- Assets/Project/Programmer/NSJ/Script/State/DrainState.cs
+++ Assets/Project/Programmer/NSJ/Script/State/DrainState.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DrainState : PlayerState
@@ -17,16 +15,31 @@ public class DrainState : PlayerState
         Player.Rb.velocity = Vector3.zero;
         View.SetBool(PlayerView.Parameter.Drain, true);
         _drainField.SetActive(true);
-        _drainField.transform.localScale = new Vector3(_drainDistance, _drainField.transform.localScale.y, _drainDistance);
+        _drainField.transform.localScale = new Vector3(0, _drainField.transform.localScale.y, 0);
     }
 
     public override void Update()
     {
         CheckInput();
+
+        // 크기가 점점 커지도록
+        float curDistance = _drainField.transform.localScale.x;
+        if (curDistance < _drainDistance)
+        {
+            _drainField.transform.localScale = new Vector3(
+                curDistance + _drainDistance * Time.deltaTime,
+                _drainField.transform.localScale.y,
+                curDistance + _drainDistance * Time.deltaTime);
+        }
+
     }
 
     public override void Exit()
-    { 
+    {
+        Player.DrainField.SetActive(false);
+    }
+    public override void OnTrigger()
+    {
         Player.DrainField.SetActive(false);
     }
 
