dc609422b3d1397b24060f278f39d05ebe912a58
anMois <kew1672@naver.com>
2024-12-24 20:12:09 +0900

    [Chore] 좀비 프리팹 수정

--- Assets/Project/Programmer/PJS/Scripts/Enemy/BaseEnemy.cs
+++ Assets/Project/Programmer/PJS/Scripts/Enemy/BaseEnemy.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 [System.Serializable]
 public class State
 {
-    [Range(50, 1000)] public int MaxHp;  // 체력
+    [Range(50, 3000)] public int MaxHp;  // 체력
     [Range(0, 20)] public int Atk;       // 공격력
     [Range(0, 10)] public float Def;    // 방어력
     [Range(0, 10)] public float Speed;    // 이동 속도
