58c1eac10657fea099bba0ff40009b82aa8d7188
tjsp9830 <mcgr1998@gmail.com>
2024-12-26 15:51:28 +0900

    [Chore] 낙석함정 로직 변경

--- Assets/Project/Programmer/LSH/Scripts/TrapStalactite.cs
+++ Assets/Project/Programmer/LSH/Scripts/TrapStalactite.cs
@@ -1,39 +1,63 @@
+using BehaviorDesigner.Runtime.Tasks;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TrapStalactite : MonoBehaviour
 {
-    [SerializeField] GameObject ray;
-    RaycastHit hit;
-    [SerializeField] float maxDistance = 15f;
+    // 레이로 검사할때 변수
+    //[SerializeField] GameObject ray;
+    //RaycastHit hit;
+    //[SerializeField] float maxDistance = 15f;
+    
+    //레이를 쏘고있다가 or 박스 영역 안에
+    //플레이어가 들어오면 가시 내림
+    //가시는 떨어지기 시작하면 2초뒤에 사라짐
+    //가시는 플레이어랑 부딪히면 체력 -40깎게함
 
     [SerializeField] GameObject stalactite;
+    [SerializeField] GameObject boxArea;
+
     [SerializeField] Transform stalactiteBeforeMove;
     [SerializeField] Transform stalactiteAfterMove;
-    //레이를 쏘고있다가
-    //플레이어가 들어오면 가시 내림
-    //가시는 플레이어랑 부딪히면 체력 -40깎게함
-    //가시는 5초뒤면 사라짐
+
+    Coroutine deleteRoutine;
+    WaitForSeconds wait;
+    [SerializeField] float deleteCount;
 
 
-    private void Update()
+    // 레이로 검사하는 로직
+    //private void Update()
+    //{
+    //    Debug.DrawRay(ray.transform.position, Vector3.down, Color.red);
+    //    Debug.DrawLine(ray.transform.position, Vector3.down, Color.red);
+    //    if (Physics.Raycast(ray.transform.position, Vector3.down, out hit, maxDistance))
+    //    {
+    //        if (hit.collider.gameObject.tag == Tag.Player)
+    //        {
+    //            stalactite.transform.position = 
+    //                Vector3.MoveTowards(stalactiteBeforeMove.position, stalactiteAfterMove.position, 5f);
+    //        }
+
+    //    }
+
+
+    //}
+
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.DrawRay(ray.transform.position, Vector3.down, Color.red);
-        Debug.DrawLine(ray.transform.position, Vector3.down, Color.red);
-        if (Physics.Raycast(ray.transform.position, Vector3.down, out hit, maxDistance))
+        if (other.gameObject.tag == Tag.Player)
         {
-            if (hit.collider.gameObject.tag == Tag.Player)
-            {
-                //TODO: 천천히 떨어지게 고치기
-                stalactite.transform.position = 
-                    Vector3.MoveTowards(stalactiteBeforeMove.position, stalactiteAfterMove.position, 5f);
-            }
-            
+            stalactite.GetComponent<Rigidbody>().isKinematic = false;
+            Coroutine deleteRoutine = StartCoroutine(DeleteRoutine());
         }
-
-
     }
 
+    
+    IEnumerator DeleteRoutine()
+    {
+        wait = new WaitForSeconds(deleteCount);
+        yield return wait;
+        stalactite.SetActive(false);
+    }
 
 }
