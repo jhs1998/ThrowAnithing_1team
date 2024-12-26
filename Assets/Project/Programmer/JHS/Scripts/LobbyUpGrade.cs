9a1b26fa641fbd888f02fcec46e952b00b6e736b
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-12-26 09:15:55 +0900

    [feat] 로비 강화 로직 프로토 타이핑

--- Assets/Project/Programmer/JHS/Scripts/LobbyUpGrade.cs
+++ Assets/Project/Programmer/JHS/Scripts/LobbyUpGrade.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class LobbyUpGrade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // GlobalGameData의 BuyUpgradeSlot에서 참 거짓을 받아와 참일경우 에만  업그레이드 
+    /*public void IncreaseStat(string stat, int amount)
     {
-        
+        switch (stat)
+        {
+            case "HP":
+                UserDataManager.instance.nowPlayer.maxHp += amount;
+                break;
+            case "Speed":
+                UserDataManager.instance.nowPlayer.speed += amount;
+                break;
+            case "Attack":
+                UserDataManager.instance.nowPlayer.attackDamage += amount;
+                break;
+            case "Luck":
+                UserDataManager.instance.nowPlayer.luck += amount;
+                break;
+            case "Coin":
+                UserDataManager.instance.nowPlayer.coin += amount;
+                break;
+            default:
+                Debug.LogWarning("Invalid stat type.");
+                break;
+        }
+        UpdateSlotUI.instance.UpdateUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void HpUpgrade(int amount)
     {
-        
+        // 지불금
+        int payment = 100;
+        if (UserDataManager.instance.nowPlayer.maxHp >= payment)
+        {
+            UserDataManager.instance.nowPlayer.coin -= payment;
+            UserDataManager.instance.nowPlayer.maxHp += amount;
+            UpdateSlotUI.instance.UpdateUI();
+        }
+        else
+        {
+            Debug.Log("coin 부족");
+        }
     }
+    public void SpeedUpgrade(int amount)
+    {
+        // 지불금
+        int payment = 100;
+        if (UserDataManager.instance.nowPlayer.coin >= payment)
+        {
+            UserDataManager.instance.nowPlayer.coin -= payment;
+            UserDataManager.instance.nowPlayer.speed += amount;
+            UpdateSlotUI.instance.UpdateUI();
+        }
+        else
+        {
+            Debug.Log("coin 부족");
+        }
+    }
+    public void CoinUpgrade(int amount)
+    {
+        UserDataManager.instance.nowPlayer.coin += amount;
+        UpdateSlotUI.instance.UpdateUI();
+    }*/
 }
