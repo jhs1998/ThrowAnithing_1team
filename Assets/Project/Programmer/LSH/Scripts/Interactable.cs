5c7d51ec3400e89773c4d96c9dd9ff712e4c38e5
tjsp9830 <mcgr1998@gmail.com>
2024-12-26 18:28:25 +0900

    [Feat]: 상호작용시 랜덤 아이템 생성하는 오브젝트 구현

--- Assets/Project/Programmer/LSH/Scripts/Interactable.cs
+++ Assets/Project/Programmer/LSH/Scripts/Interactable.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    [Range(2f, 5f)] [SerializeField] float overlapSphereRadius;
-    Collider[] hitColliders;
+
+    [Range(2f, 5f)] [SerializeField] float overlapSphereRadius; //보물상자 근처 범위
+    [Range(2f, 5f)] [SerializeField] float playerAreaRadius; //플레이어 근처 랜덤생성될때 근처 범위
+    Collider[] hitColliders; //해당 물체에 닿는 모든 콜라이더를 넣어둘 배열
+    Collider playerCollider; //그중에서 플레이어 콜라이더만 저장할 변수
 
     [SerializeField] bool isInSphere; //T오버랩스피어에닿아있음 F아님
     [SerializeField] GameObject pressKeyUI;
+
+    [SerializeField] GameObject[] itemPrefabs;
+    Vector3 itemRandomSpawnArea;
 
     private void Start()
     {
@@ -19,13 +25,13 @@ public class Interactable : MonoBehaviour
 
     private void Update()
     {
-        
         hitColliders = Physics.OverlapSphere(transform.position, overlapSphereRadius);
 
         for (int i = 0; i < hitColliders.Length; i++)
         {
             if (hitColliders[i].gameObject.tag == Tag.Player)
             {
+                playerCollider = hitColliders[i];
                 isInSphere = true;
                 pressKeyUI.SetActive(true);
             }
@@ -41,11 +47,37 @@ public class Interactable : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                Debug.Log("아이템이 이 주변에 랜덤으로 튀어나옴");
+                SettingSpawnArea();
+                DropRandomItems();
+                SetFalseObject(); 
             }
         }
 
     }
 
 
+    //랜덤으로 스폰될 영역 설정
+    public void SettingSpawnArea()
+    {
+        float range_x = playerCollider.bounds.size.x * playerAreaRadius;
+        float range_z = playerCollider.bounds.size.z * playerAreaRadius;
+        range_x = Random.Range((range_x / 2) * -1, range_x / 2);
+        range_z = Random.Range((range_z / 2) * -1, range_z / 2);
+        itemRandomSpawnArea = new Vector3(range_x, 0.5f, range_z);
+    }
+
+    //랜덤으로 스폰될 아이템 설정 후 생성
+    public void DropRandomItems()
+    {        
+        int itemRandom = Random.Range(0, itemPrefabs.Length);
+        Instantiate(itemPrefabs[itemRandom], itemRandomSpawnArea, Quaternion.identity);
+
+
+    }
+
+    //생성시 보물상자 아이템은 사라짐
+    public void SetFalseObject()
+    {
+        gameObject.SetActive(false);
+    }
 }
