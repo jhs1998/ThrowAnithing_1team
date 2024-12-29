9a6d97bd8c8ee2df16e09dde75c374b712a5921f
anMois <kew1672@naver.com>
2024-12-29 17:34:35 +0900

    [Fix] 이슈 버그 수정

--- Assets/Project/Programmer/PJS/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Project/Programmer/PJS/Scripts/Enemy/MeleeEnemy.cs
@@ -7,19 +7,21 @@ public class MeleeEnemy : BaseEnemy
 {
     [SerializeField] CapsuleCollider attakArm;
 
+    private bool _canAttack;
+
     public void BeginAttack()
     {
-        attakArm.isTrigger = true;
+        _canAttack = true;
     }
 
     public void EndAttack()
     {
-        attakArm.isTrigger = false;
+        _canAttack = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag == Tag.Player)
+        if (other.transform.tag == Tag.Player && _canAttack == true)
         {
             IHit hit = other.transform.GetComponent<IHit>();
             hit.TakeDamage(Damage, true);
