a2e4f4c071e1464581c633d1dd512675afa7600d
LeeJaeHo-96 <towpam123@gmail.com>
2025-01-04 13:56:08 +0900

    Feat : 미니맵 로직 수정

--- Assets/Project/Programmer/NSJ/Script/TagLayer.cs
+++ Assets/Project/Programmer/NSJ/Script/TagLayer.cs
@@ -10,7 +10,7 @@ public static partial class Tag
     public const string Respawn = "Respawn";
     public const string Finish = "Finish";
     public const string EditorOnly = "EditorOnly";
-    public const string MainCamera = "MainCamara";
+    public const string MainCamera = "MainCamera";
     public const string Player = "Player";
     public const string GameController = "GameController";
     public const string Monster = "Monster";
