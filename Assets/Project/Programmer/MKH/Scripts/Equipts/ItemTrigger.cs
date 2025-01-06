88a62d41dcb856eeef60f1a0484efdee70c8cd88
MoonKyoungHun <lsf8355@gmail.com>
2025-01-06 18:50:05 +0900

    [Feat] 아이템 획득에 대한 조건 추가
    아이템 획득에 대한 조건 추가

--- Assets/Project/Programmer/MKH/Scripts/Equipts/ItemTrigger.cs
+++ Assets/Project/Programmer/MKH/Scripts/Equipts/ItemTrigger.cs
@@ -25,12 +25,16 @@ namespace MKH
             // Item 태그 붙은 other 오브젝트
             if (other.gameObject.tag == Tag.Item)
             {
-                // 아이템 타입이 None이 아닐 시 
-                if (mCurrentItem.Item.Type != ItemType.None)
+                for (int i = 0; i < mInventory.mSlots.Length; i++)
                 {
-                    // 인벤토리에 아이템 추가
-                    mInventory.AcquireItem(mCurrentItem.Item);
-                    Destroy(other.gameObject);
+                    // 아이템 타입이 None이 아니고 인벤토리 슬롯이 null 일 때
+                    if (mCurrentItem.Item.Type != ItemType.None && mInventory.mSlots[i].Item == null)
+                    {
+                        // 인벤토리에 아이템 추가
+                        mInventory.AcquireItem(mCurrentItem.Item);
+                        Destroy(other.gameObject);
+                        return;
+                    }
                 }
             }
         }
