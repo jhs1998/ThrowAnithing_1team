f89bf2e01616599e855eae6ba1033979986e6769
tjsp9830 <mcgr1998@gmail.com>
2025-01-08 20:12:49 +0900

    [Fix] 작동 로직 수정중

--- Assets/Project/Programmer/LSH/Scripts/SwingObject.cs
+++ Assets/Project/Programmer/LSH/Scripts/SwingObject.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class SwingObject : MonoBehaviour
 {
-    [SerializeField] float swingSpeed = 2.0f;  // 판자의 흔들리는 속도
+    [Range(1f, 5f)] [SerializeField] float swingSpeed;  // 판자의 흔들리는 속도
     [SerializeField] float swingAngle = 30.0f; // 판자의 최대 흔들리는 각도
+
+    [SerializeField] GameObject triggerArea;
+    [Range(0.5f, 1.0f)] [SerializeField] float triggerSize;
 
     void Update()
     {
@@ -20,5 +23,10 @@ public class SwingObject : MonoBehaviour
                                               angle);
 
 
+
+
+        triggerArea.GetComponent<SphereCollider>().radius = triggerSize;
     }
+
+    
 }
