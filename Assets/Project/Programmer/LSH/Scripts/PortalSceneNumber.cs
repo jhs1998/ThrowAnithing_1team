ef25e0ad5e4be71fe90de32005063286375e578e
LeeJaeHo-96 <towpam123@gmail.com>
2025-01-10 09:55:38 +0900

    Fix : 포탈 이용시 인벤토리 꺼지게 처리

--- Assets/Project/Programmer/LSH/Scripts/PortalSceneNumber.cs
+++ Assets/Project/Programmer/LSH/Scripts/PortalSceneNumber.cs
@@ -7,8 +7,13 @@ public class PortalSceneNumber : MonoBehaviour
     [SerializeField] public SceneField nextScene;
     [SerializeField] public SceneField[] hiddenSceneArr;
 
+    [SerializeField] GameObject[] inventory;
+
     private void Start()
     {
+        for(int i = 0; i < inventory.Length; i++)
+            inventory[i].SetActive(false);
+
         ToStage._ToStage();
     }
 
