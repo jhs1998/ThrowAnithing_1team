776850c8ad7dc3af4b593b4b058e8d1e350af6cc
MoonKyoungHun <lsf8355@gmail.com>
2025-01-10 20:10:10 +0900

    [Feat] 아이템 이펙트 생성
    몬스터 죽은 후 아이템 생성 및 소멸 이펙트 생성

--- Assets/Project/Programmer/NSJ/Script/DataContainer.cs
+++ Assets/Project/Programmer/NSJ/Script/DataContainer.cs
@@ -16,7 +16,7 @@ public class DataContainer : MonoBehaviour
     /// 블루칩 데이터
     /// </summary>
     public static AdditionalEffect[] BlueChips { get { return Instance._blueChips; } }
-    [SerializeField]private TestBlueChip _blueChipItem;
+    [SerializeField] private TestBlueChip _blueChipItem;
     public static TestBlueChip BlueChipItem { get { return Instance._blueChipItem; } }
     [System.Serializable]
     public struct ItemStruct
@@ -41,6 +41,18 @@ public class DataContainer : MonoBehaviour
     public static ItemStruct Items { get { return Instance._items; } }
     public static List<DropList> ItemList = new List<DropList>();
 
+    [SerializeField] GameObject[] _itemPaticle;
+    /// <summary>
+    /// 아이템 파티클 데이터
+    /// </summary>
+    public static GameObject[] ItemPaticle { get { return Instance._itemPaticle; } }
+
+    [SerializeField] float _destroyItemTime = 0;
+    /// <summary>
+    /// 아이템 사라지는 시간
+    /// </summary>
+    public static float DestroyItemTime { get { return Instance._destroyItemTime; } }
+
     /// <summary>
     /// 투척물 데이터
     /// </summary>
@@ -57,20 +69,21 @@ public class DataContainer : MonoBehaviour
 
     [SerializeField] DamageText[] _damageTexts;
     private Dictionary<CrowdControlType, DamageText> _damageTextDic = new Dictionary<CrowdControlType, DamageText>();
-    private static Dictionary<CrowdControlType,DamageText> s_damageTextDic { get { return Instance._damageTextDic; } }
+    private static Dictionary<CrowdControlType, DamageText> s_damageTextDic { get { return Instance._damageTextDic; } }
     private void Awake()
     {
+        Instance = this;
         InitSingleTon();
-   
+
         foreach (ThrowObject throwObject in _throwObjects)
         {
             _throwObjectDic.Add(throwObject.Data.ID, throwObject);
         }
-        foreach(ArmUnit armUnit in _armUnits)
+        foreach (ArmUnit armUnit in _armUnits)
         {
             _armUnitDic.Add(armUnit.ArmType, armUnit);
         }
-        foreach(DamageText damageText in _damageTexts)
+        foreach (DamageText damageText in _damageTexts)
         {
             _damageTextDic.Add(damageText.Type, damageText);
         }
@@ -84,7 +97,7 @@ public class DataContainer : MonoBehaviour
     /// </summary>
     public static ThrowObject GetThrowObject(int ID)
     {
-        return  s_ThrowObjectDic[ID];
+        return s_ThrowObjectDic[ID];
     }
     /// <summary>
     /// 암유닛 얻기
@@ -107,9 +120,10 @@ public class DataContainer : MonoBehaviour
     {
         return dropList.itemList[Random.Range(0, dropList.Count)];
     }
-    public static GameObject GetItemPrefab()
+    public static GameObject GetItemPrefab(Vector3 pos)
     {
         DropList dropList = ItemList[Random.Range(0, ItemList.Count)];
+        CoroutineHandler.StartRoutine(CreateItem(pos, dropList));
         return dropList.itemList[Random.Range(0, dropList.Count)];
     }
 
@@ -130,7 +144,7 @@ public class DataContainer : MonoBehaviour
     }
     private void InitSingleTon()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
             transform.SetParent(null);
@@ -139,6 +153,23 @@ public class DataContainer : MonoBehaviour
         else
         {
             Destroy(gameObject);
+        }
+    }
+
+    static IEnumerator CreateItem(Vector3 pos, DropList dropList)
+    {
+        GameObject startEffect = ObjectPool.GetPool(ItemPaticle[0], pos, Quaternion.Euler(-90f, 0, 0));
+        yield return 0.3f.GetDelay();
+        ObjectPool.ReturnPool(startEffect);
+        GameObject dropPrefab = dropList.itemList[Random.Range(0, dropList.Count)];
+        GameObject obj = Instantiate(dropPrefab, pos + new Vector3(0, 1, 0), Quaternion.identity);
+        if (dropPrefab != null)
+        {
+            yield return Instance._destroyItemTime.GetDelay();
+            Destroy(obj);
+            GameObject endEffect = ObjectPool.GetPool(ItemPaticle[1], pos + new Vector3(0, 1, 0), Quaternion.Euler(-90f, 0, 0));
+            yield return 0.5f.GetDelay();
+            ObjectPool.ReturnPool(endEffect);
         }
     }
 }
