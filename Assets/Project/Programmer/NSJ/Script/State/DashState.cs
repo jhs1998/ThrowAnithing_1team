e4a8665800d16f4c0feffa583e20fdbfed9895d7
SeungJu0406 <nss8164@gmail.com>
2025-01-11 16:07:35 +0900

    [Fix] 무한 대쉬 버그 해결

--- Assets/Project/Programmer/NSJ/Script/State/DashState.cs
+++ Assets/Project/Programmer/NSJ/Script/State/DashState.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class DashState : PlayerState
 {
-   //float _timeBuffer;
+    //float _timeBuffer;
     bool _isDashEnd;
     Vector3 _startPos;
     Coroutine _checkInputRoutine;
+    Coroutine _checkCanMove;
+    Vector3 _prevPos;
+    bool canMove = true;
     public DashState(PlayerController controller) : base(controller)
     {
         UseStamina = true;
-       
+
     }
     public override void InitArm()
     {
@@ -25,10 +27,16 @@ public class DashState : PlayerState
         Player.IsInvincible = true;
         Player.LookAtMoveDir();
         View.SetTrigger(PlayerView.Parameter.Dash);
+
+        _checkCanMove = CoroutineHandler.StartRoutine(_checkCanMove, CheckCanMove());
+
     }
     public override void Exit()
     {
+        _checkCanMove = CoroutineHandler.StopRoutine(_checkCanMove);
+
         Player.IsInvincible = false;
+        canMove = true;
     }
     public override void Update()
     {
@@ -56,17 +64,30 @@ public class DashState : PlayerState
     /// </summary>
     public void Dash()
     {
-        if (Vector3.Distance(transform.position, _startPos) < Model.DashDistance && Player.IsWall ==false)
+        if (canMove == false && _isDashEnd == false)
         {
-            Rb.velocity = transform.forward * Model.MoveSpeed * 2;
+            EndDash();
+            return;
         }
-        else if(_isDashEnd == false)
+        else
         {
-            _isDashEnd = true;
-            View.SetTrigger(PlayerView.Parameter.DashEnd);
-            //Rb.velocity = transform.forward * Model.MoveSpeed;
-            CoroutineHandler.StartRoutine(DashEndRoutine());
+            if (Vector3.Distance(transform.position, _startPos) < Model.DashDistance && Player.IsWall == false)
+            {
+                Rb.velocity = transform.forward * Model.MoveSpeed * 2;
+            }
+            else if (_isDashEnd == false)
+            {
+                EndDash();
+            }
         }
+    }
+
+    private void EndDash()
+    {
+        _isDashEnd = true;
+        View.SetTrigger(PlayerView.Parameter.DashEnd);
+        //Rb.velocity = transform.forward * Model.MoveSpeed;
+        CoroutineHandler.StartRoutine(DashEndRoutine());
     }
 
     IEnumerator DashEndRoutine()
@@ -76,11 +97,25 @@ public class DashState : PlayerState
         {
             Rb.velocity = transform.forward * speed;
             speed -= Time.deltaTime * 20f;
-            if(speed < 0)
+            if (speed < 0)
             {
                 yield break;
             }
             yield return null;
+        }
+    }
+
+    IEnumerator CheckCanMove()
+    {
+        canMove = true;
+        while (true)
+        {
+            yield return 0.1f.GetDelay();
+            if (Vector3.Distance(_prevPos, transform.position) < 0.05f)
+            {
+                canMove = false;
+            }
+            _prevPos = transform.position;
         }
     }
 }
