2da289c3eda5799353a05aced6c64263a2bbbbf9
SeungJu0406 <nss8164@gmail.com>
2025-01-15 10:14:13 +0900

    [Fix] 투척물 재배치

--- Assets/Project/Programmer/LSH/Scripts/SpawnPrefabs.cs
+++ Assets/Project/Programmer/LSH/Scripts/SpawnPrefabs.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnPrefabs : MonoBehaviour
@@ -6,18 +7,14 @@ public class SpawnPrefabs : MonoBehaviour
     [SerializeField] GameObject prefabs; //스폰될 프리팹
     [SerializeField] List<Transform> spawnPos; //스폰 지점들
     [SerializeField] int spwanCount; //지점들 중에서 몇개를 스폰시킬지
-    Transform[] childCube; //prefabs가 Tag.Trash일때의 자식들
-    [SerializeField] bool isAlreadySpawn;
 
+    [SerializeField] int trashCount; // 쓰레기 몇개 스폰?
+    ThrowObject[] childCube; //prefabs가 Tag.Trash일때의 자식들
+    [SerializeField] bool isAlreadySpawn;
 
     private void Start()
     {
-
-        childCube = prefabs.GetComponentsInChildren<Transform>(true);
-
         SpawnPrefabPos();
-
-
     }
 
 
@@ -35,11 +32,11 @@ public class SpawnPrefabs : MonoBehaviour
             {
 
                 int num = Random.Range(0, spawnPos.Count);
-                Instantiate(prefabs, spawnPos[num]);
+                GameObject instance = Instantiate(prefabs, spawnPos[num]);
 
                 if (prefabs.tag == Tag.Trash)
                 {
-                    SpawnTrashPos();
+                    SpawnTrashPos(instance);
                 }
 
                 spawnPos.Remove(spawnPos[num]);
@@ -52,15 +49,49 @@ public class SpawnPrefabs : MonoBehaviour
     }
 
 
-    public void SpawnTrashPos()
+    public void SpawnTrashPos(GameObject instance)
     {
 
-        for (int i = 0; i < childCube.Length; i++)
+        //for (int i = 0; i < childCube.Length; i++)
+        //{
+        //    if (childCube[i] == prefabs.transform)
+        //        childCube[i].gameObject.SetActive(true);
+        //    else
+        //        childCube[i].gameObject.SetActive(Random.value > 0.5f);
+
+        //}
+
+        childCube = instance.GetComponentsInChildren<ThrowObject>(true);
+
+        if (trashCount >= childCube.Length)
         {
-            if (childCube[i] == prefabs.transform)
-                childCube[i].gameObject.SetActive(true);
-            else
-                childCube[i].gameObject.SetActive(Random.value > 0.5f);
+            trashCount = childCube.Length;
+        }
+
+        foreach (ThrowObject child in childCube) 
+        {
+            child.gameObject.SetActive(false);
+        }
+
+
+        for (int i = 0; i < trashCount; i++)
+        {
+            while (true)
+            {
+                int randomNum = Random.Range(0, childCube.Length);
+
+                if (childCube[randomNum].gameObject.activeSelf == true) 
+                {
+                    continue;
+                }
+                else
+                {
+                    Debug.Log($"{childCube[randomNum].gameObject.name} 통과 ");
+                    childCube[randomNum].gameObject.SetActive (true);
+                    break;
+                }
+            }
+
 
         }
 
