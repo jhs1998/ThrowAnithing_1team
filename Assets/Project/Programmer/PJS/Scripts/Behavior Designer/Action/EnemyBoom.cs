a1d3b8d9fa842257859c4bf7ad14140d85dd6498
SeungJu0406 <nss8164@gmail.com>
2025-01-17 14:36:12 +0900

    [Fix] 디스트로이어 수정

--- Assets/Project/Programmer/PJS/Scripts/Behavior Designer/Action/EnemyBoom.cs
+++ Assets/Project/Programmer/PJS/Scripts/Behavior Designer/Action/EnemyBoom.cs
@@ -12,6 +12,8 @@ public class EnemyBoom : BaseAction
 
     public override void OnAwake()
     {
+        baseEnemy = GetComponent<BaseEnemy>();
+
         foreach (AudioClip clip in baseEnemy.GetDaethClips())
         {
             deathClips.Add(clip);
